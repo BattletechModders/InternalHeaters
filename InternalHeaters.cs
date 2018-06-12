8be96b4d6022442b6be3feacc4f715c6de95626f
Joel Meador <joel@expectedbehavior.com>
2018-06-12 17:49:53 -0400

    add some settings to allow chassis heatsinks or double heatsink features to be disabled, and make double heat sinks only double engine output when all heatsinks installed on a mech are doubles

--- InternalHeaters.cs
+++ InternalHeaters.cs
@@ -7,10 +7,11 @@ using Newtonsoft.Json;
 
 namespace InternalHeaters
 {
-    public class AssemblyPatch 
+    public class AssemblyPatch
     {
         internal static Settings ModSettings = new Settings();
         internal static string ModDirectory;
+
         public static void Init(string directory, string settingsJSON)
         {
             ModDirectory = directory;
@@ -23,11 +24,12 @@ namespace InternalHeaters
                 Logger.Error(ex);
                 ModSettings = new Settings();
             }
+
             var harmony = HarmonyInstance.Create($"com.joelmeador.{Settings.ModName}");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
-    
+
     [HarmonyPatch(typeof(Mech), "GetHeatSinkDissipation")]
     public static class Mech_GetHeatSinkDissipation_Patch
     {
@@ -37,15 +39,27 @@ namespace InternalHeaters
             Logger.Debug($"Patching in\npreheated: {__result}");
             Logger.Debug($"hsc? {__instance.StatCollection.GetValue<int>("HeatSinkCapacity")}");
             var extraEngineDissipation = 0f;
-            if (mech.allComponents.Any(component => component.Description.Id == "Gear_HeatSink_Generic_Double"))
+            if (AssemblyPatch.ModSettings.AllDoubleHeatSinksDoubleEngineHeatDissipation &&
+                mech.allComponents
+                    .FindAll(component => component.componentType == ComponentType.HeatSink)
+                    .All(component => component.Description.Id == "Gear_HeatSink_Generic_Double"))
             {
                 extraEngineDissipation = __instance.Combat.Constants.Heat.DefaultHeatSinkDissipationCapacity *
                                          __instance.Combat.Constants.Heat.InternalHeatSinkCount;
             }
-            var additionalHeatSinks = __instance.MechDef.Chassis.Heatsinks;
-            var heatSinkDissipation = __instance.Combat.Constants.Heat.DefaultHeatSinkDissipationCapacity;
-            var additionalHeatSinkDissipation = ((float) additionalHeatSinks * heatSinkDissipation) + extraEngineDissipation;
-            Logger.Debug($"additionalHeatSinks: {additionalHeatSinks}\nheatsinkDissipation: {heatSinkDissipation}\naddtionalHeatSinkDissipation: {additionalHeatSinkDissipation}\nextraEngineDissipation: {extraEngineDissipation}");
+
+            var additionalHeatSinks = 0;
+            var heatSinkDissipation = 0f;
+
+            if (AssemblyPatch.ModSettings.UseChassisHeatSinks)
+            {
+                additionalHeatSinks = __instance.MechDef.Chassis.Heatsinks;
+                heatSinkDissipation = __instance.Combat.Constants.Heat.DefaultHeatSinkDissipationCapacity;
+            }
+
+            var additionalHeatSinkDissipation = (additionalHeatSinks * heatSinkDissipation) + extraEngineDissipation;
+            Logger.Debug(
+                $"additionalHeatSinks: {additionalHeatSinks}\nheatsinkDissipation: {heatSinkDissipation}\naddtionalHeatSinkDissipation: {additionalHeatSinkDissipation}\nextraEngineDissipation: {extraEngineDissipation}");
             __result = __result + additionalHeatSinkDissipation;
             Logger.Debug($"reheated: {__result}");
         }
--- Settings.cs
+++ Settings.cs
@@ -5,5 +5,11 @@
         public const string ModName = "InternalHeaters";
 
         public bool debug = false;
+
+        public bool useChassisHeatSinks = false;
+        public bool UseChassisHeatSinks => useChassisHeatSinks;
+
+        public bool allDoubleHeatSinksDoubleEngineHeatDissipation = false;
+        public bool AllDoubleHeatSinksDoubleEngineHeatDissipation => allDoubleHeatSinksDoubleEngineHeatDissipation;
     }
 }
