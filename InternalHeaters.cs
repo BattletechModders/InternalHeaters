bdc9002c30648b3141a310b6910c57a9c7608930
Joel Meador <joel@expectedbehavior.com>
2018-06-15 14:20:53 -0400

    copy some heat efficiency bar changes from https://github.com/CptMoore/StatsFixMod to make it more obvious when the single/double is happening and also take into account heat efficiency of double engine in mech lab

--- InternalHeaters.cs
+++ InternalHeaters.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using BattleTech;
 using Harmony;
 using Newtonsoft.Json;
+using UnityEngine;
 
 namespace InternalHeaters
 {
@@ -69,6 +70,154 @@ namespace InternalHeaters
                 $"additionalHeatSinks: {additionalHeatSinks}\nheatsinkDissipation: {heatSinkDissipation}\naddtionalHeatSinkDissipation: {additionalHeatSinkDissipation}\nextraEngineDissipation: {extraEngineDissipation}");
             __result = __result + additionalHeatSinkDissipation;
             Logger.Debug($"reheated: {__result}");
+        }
+    }
+
+    // Stuff below is copied almost directly from https://github.com/CptMoore/StatsFixMod
+    [HarmonyPatch(typeof(MechStatisticsRules), "CalculateHeatEfficiencyStat")]
+    public static class MechStatisticsRulesCalculateHeatEfficiencyStatPatch
+    {
+        private static CombatGameConstants Combat;
+
+        public static bool Prefix(MechDef mechDef, ref float currentValue, ref float maxValue)
+        {
+            try
+            {
+                if (Combat == null)
+                {
+                    Combat = CombatGameConstants.CreateFromSaved(UnityGameInstance.BattleTechGame);
+                }
+                var baseHeatSinkDissipation = Combat.Heat.InternalHeatSinkCount * Combat.Heat.DefaultHeatSinkDissipationCapacity;
+                var totalHeatSinkDissipation = baseHeatSinkDissipation + Calculators.DoubleHeatsinkEngineDissipation(mechDef, Combat.Heat);
+                var heatGenerationWeapons = 0f;
+                var numberOfJumpJets = 0;
+
+                foreach (var mechComponentRef in mechDef.Inventory)
+                {
+                    if (mechComponentRef.Def == null)
+                    {
+                        mechComponentRef.RefreshComponentDef();
+                    }
+                    if (mechComponentRef.Def is WeaponDef)
+                    {
+                        var weaponDef = (WeaponDef)mechComponentRef.Def;
+                        heatGenerationWeapons += weaponDef.HeatGenerated;
+                    }
+                    else if (mechComponentRef.ComponentDefType == ComponentType.JumpJet)
+                    {
+                        if (mechComponentRef.DamageLevel < ComponentDamageLevel.NonFunctional)
+                        {
+                            numberOfJumpJets++;
+                        }
+                    }
+                    else if (mechComponentRef.Def is HeatSinkDef)
+                    {
+                        var heatSinkDef = (HeatSinkDef)mechComponentRef.Def;
+                        totalHeatSinkDissipation += heatSinkDef.DissipationCapacity;
+                    }
+                }
+
+                Logger.Debug($"heatGenerationWeapons: {heatGenerationWeapons}");
+                Logger.Debug($"totalHeatSinkDissipation: {totalHeatSinkDissipation}");
+
+                var maxHeat = Combat.Heat.MaxHeat;
+                {
+                    var stats = new StatCollection();
+                    var maxHeatStatistic = stats.AddStatistic("MaxHeat", maxHeat);
+                    var heatGeneratedStatistic = stats.AddStatistic("HeatGenerated", heatGenerationWeapons);
+
+                    foreach (var mechComponentRef in mechDef.Inventory)
+                    {
+                        if (mechComponentRef.Def == null || mechComponentRef.Def.statusEffects == null)
+                        {
+                            continue;
+                        }
+
+                        var statusEffects = mechComponentRef.Def.statusEffects;
+                        foreach (var effect in statusEffects)
+                        {
+                            switch (effect.statisticData.statName)
+                            {
+                                case "MaxHeat":
+                                    stats.PerformOperation(maxHeatStatistic, effect.statisticData);
+                                    break;
+                                case "HeatGenerated":
+                                    if (effect.statisticData.targetCollection == StatisticEffectData.TargetCollection.Weapon)
+                                    {
+                                        stats.PerformOperation(heatGeneratedStatistic, effect.statisticData);
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                    
+                    maxHeat = maxHeatStatistic.CurrentValue.Value<int>();
+                    heatGenerationWeapons = heatGeneratedStatistic.CurrentValue.Value<float>();
+                }
+
+                Logger.Debug($"maxHeat: {maxHeat}");
+                Logger.Debug($"heatGenerationWeapons: {heatGenerationWeapons}");
+
+                if (numberOfJumpJets >= Combat.MoveConstants.MoveTable.Length)
+                {
+                    numberOfJumpJets = Combat.MoveConstants.MoveTable.Length - 1;
+                }
+
+                var heatGenerationJumpJets = 0f;
+                var jumpHeatDivisor = 3;
+                if (numberOfJumpJets > 0)
+                {
+                    heatGenerationJumpJets += numberOfJumpJets * Combat.Heat.JumpHeatUnitSize / jumpHeatDivisor;
+                }
+                else
+                {
+                    heatGenerationJumpJets = 0f;
+                }
+
+                totalHeatSinkDissipation *= Combat.Heat.GlobalHeatSinkMultiplier;
+                var totalHeatGeneration = (heatGenerationWeapons + heatGenerationJumpJets) * Combat.Heat.GlobalHeatIncreaseMultiplier;
+
+                Logger.Debug($"totalHeatGeneration: {totalHeatGeneration}");
+
+                // rounding steps for heatSinkDissipation
+                var heatDissipationPercent = Mathf.Min(totalHeatSinkDissipation / totalHeatGeneration * 100f, UnityGameInstance.BattleTechGame.MechStatisticsConstants.MaxHeatEfficiency);
+                heatDissipationPercent = Mathf.Max(heatDissipationPercent, UnityGameInstance.BattleTechGame.MechStatisticsConstants.MinHeatEfficiency);
+
+                Logger.Debug($"heatDissipationPercent: {heatDissipationPercent}");
+
+                totalHeatSinkDissipation = totalHeatGeneration * (heatDissipationPercent / 100f);
+
+                Logger.Debug($"totalHeatSinkDissipation: {totalHeatSinkDissipation}");
+
+                var heatLeftOver = totalHeatGeneration - totalHeatSinkDissipation;
+                var unusedHeatCapacity = maxHeat - heatLeftOver;
+
+                Logger.Debug($"heatLeftOver: {heatLeftOver}");
+                Logger.Debug($"unusedHeatCapacity: {unusedHeatCapacity}");
+
+                currentValue = Mathf.Round((unusedHeatCapacity / maxHeat) * 10f);
+                currentValue = Mathf.Max(Mathf.Min(currentValue, 10f), 1f);
+                maxValue = 10f;
+                return false;
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+                return true;
+            }
+        }
+    }
+
+    internal static class Utility
+    {
+        internal static void PerformOperation(this StatCollection collection, Statistic statistic,
+            StatisticEffectData data)
+        {
+            var type = Type.GetType(data.modType);
+            var variant = new Variant(type);
+            variant.SetValue(data.modValue);
+            variant.statName = data.statName;
+            collection.PerformOperation(statistic, data.operation, variant);
         }
     }
 }
